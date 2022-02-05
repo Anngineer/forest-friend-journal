0d467c9a80ed29a95f7314b0270166935fc3df1f
Ann <anngineer@gmail.com>
2022-02-05 16:13:03 -0500

    Adds js for refreshing form and event listeners to submit the form as well as css for the journal section

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -18,7 +18,7 @@ const form = document.querySelector("form");
 const createButton = document.querySelector("#submit-button");
 const linkToShare = document.querySelector("#link-to-share");
 const messageHyperlink = document.querySelector("#message-hyperlink");
-
+const linkContainer = document.querySelector("#link-container");
 // Event listener to make the link if you hit ENTER
 //We're doing this because the hex can't handle <br> at this level
 messageInput.addEventListener("keyup", ({ key }) => {
@@ -36,18 +36,30 @@ form.addEventListener("submit", (e) => {
 
 function createMessage() {
   let message = messageInput.value;
+  // Get rid of any line breaks.
+  messageNoBreak = message.replace(/(\r\n|\n|\r)/gm, " ");
+  console.log(messageNoBreak);
   // Encode the message
-  let encoded = btoa(message);
+  let encoded = btoa(messageNoBreak);
   console.log(`${message} becomes ${encoded}`);
-  // linkToShare.value = encoded;
-  // Our location for this website
-  // NOTE: so, this is using our current url but in the future
-  // it will be using the url for the decoder page instead
   let myURL = "https://anngineer.github.io/ffj-entry/";
   console.log(myURL);
+
+  // Set the link url for the hyperlink to the url you created
   let newURL = `${myURL}#${encoded}`;
   linkToShare.value = newURL;
-  // Change the link to visible with a class
-  // Set the link url for the hyperlink to the url you created
+
   messageHyperlink.setAttribute("href", newURL);
+  // Change the link to visible with a class
+  linkContainer.classList = "";
+  linkToShare.focus();
+  form.reset();
+  messageInput.value = message;
 }
+
+linkToShare.addEventListener("click", () => {
+  linkToShare.select();
+});
+linkToShare.addEventListener("focus", () => {
+  linkToShare.select();
+});
