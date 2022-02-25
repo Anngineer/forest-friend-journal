21d7256de41d934021c027ca1c713f981be2c785
Ann <anngineer@gmail.com>
2022-02-25 15:36:53 -0500

    Adds a musical note image, guitar wav, and js functions to play and move the image when the journal prompt is called

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -54,16 +54,18 @@ moreInfoButton.addEventListener("click", () => {
 // JAVASCRIPT FOR THE Bear Prompt
 
 let bearPrompts = [
-  "Need any help coming up with what to write about in the journal letter? Strum the bear's guitar with a click and he'll ask you a question here for you to answer. ",
-  "If you were to dream about something tonight, what would you like it to be?",
-  "What were you able to start or complete today? Little things add up!",
-  "Did you drink water and take any medications that you’re supposed to?",
-  "Who is someone you admire?",
-  "If you could jump into any film, what would it be and why?",
+  `"Need any help coming up with what to write about in the journal letter? Strum the bear's guitar with a click and he'll ask you a question here for you to answer. "`,
+  ` "If you were to dream about something tonight, what would you like it to be?"`,
+  ` "What were you able to start or complete today? Little things add up!"`,
+  `"Did you drink water and take any medications that you’re supposed to?"`,
+  `"Who is someone you admire?"`,
+  `"If you could jump into any film, what would it be and why?"`,
 ];
-
+const guitarStrum = document.getElementById("guitar-strum");
 const bearButton = document.querySelector("#bear-button");
+const bear = document.querySelector("#bear");
 const suggestionBox = document.querySelector("#suggestion-box");
+const soundImage = document.getElementById("music-note");
 promptNumber = 0;
 bearButton.addEventListener("click", () => {
   changePrompt();
@@ -78,18 +80,62 @@ suggestionBox.addEventListener("click", () => {
 function changePrompt() {
   if (promptNumber < 5) {
     promptNumber += 1;
-    console.log(promptNumber);
+    // console.log(promptNumber);
     changePromptInnerText();
   } else {
     promptNumber = 1;
-    console.log(promptNumber);
-    console.log("Nah, no more times!");
+    // console.log(promptNumber);
+    // console.log("Nah, no more times!");
     changePromptInnerText();
   }
 }
 function changePromptInnerText() {
-  console.log(bearPrompts[promptNumber]);
   suggestionBox.innerText = bearPrompts[promptNumber];
+  guitarStrum.currentTime = 0;
+  guitarStrum.play();
+  animateMusic();
+}
+
+function animateMusic() {
+  // get the position of the bear
+  const rect = bear.getBoundingClientRect();
+  const width = (bear.offsetWidth / 3) * 2;
+  const halfHeight = bear.offsetHeight / 3;
+  let left = rect.left + width + window.scrollX;
+  let top = rect.top + halfHeight + window.scrollY;
+  console.log(rect);
+  console.log(`left is ${left}`);
+  console.log(`top is ${top}`);
+  // put that position into a function to place the music image
+  positionMusic(left, top);
+}
+
+function positionMusic(left, top) {
+  // move the image to that location
+  soundImage.style.left = `${left}px`;
+  soundImage.style.top = `${top}px`;
+  // use css to make it visible
+  soundImage.classList.remove("hidden");
+  //animate the image using js
+  floatImage(left, top);
+}
+
+var idAnimate = null;
+function floatImage(left, top) {
+  clearInterval(idAnimate);
+  let tick = 0;
+  console.log("cleared idAnimate");
+  idAnimate = setInterval(move, 1);
+  function move() {
+    if (tick === 45) {
+      clearInterval(idAnimate);
+      soundImage.classList.add("hidden");
+    } else {
+      tick++;
+      soundImage.style.left = `${left + tick}px`;
+      soundImage.style.top = `${top - tick}px`;
+    }
+  }
 }
 
 // --------------------------------------------
